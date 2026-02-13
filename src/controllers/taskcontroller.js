d1b86a780950534215f1fa8361c6117d26127373
NITHISHKUMAR0283 <nk0283@srmist.edu.in>
2026-02-13 23:24:34 +0530

    adding the crud functionalities

--- src/controllers/taskcontroller.js
+++ src/controllers/taskcontroller.js
@@ -31,5 +31,49 @@ const getTask = async (req,res)=>{
         })
     }
 }
+const getOneTask = async(req,res)=>{
+    try{
+        const task = await Task.findById(req.params.id);
+        if (task ==null){
+            return res.status(404).json({
+                success:false,
+                message:"Cant find the product"
+            })
+        }
+        res.status(200).json({
+            success:true,
+            task:task
+        })
+    }
+    catch(err){
+        res.status(500).json({
+            success:false,
+            message:`There is a error finding the task in the given id ${err}`
+            
+        })
+    }
+}
+const updateTask = async (req,res)=>{
+    try{
+    const task = await Task.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true});
+    if (task == null){
+        return res.status(404).json({
+            success:false,
+            message:"Cant find by the id given"
+        })
+    }
+    res.status(200).json({
+        success:true,
+        message:`Successfully updated the tasks`,
+        newtask:task
+    })
+    }
+    catch(err){
+        res.status(500).json({
+            success:false,
+            message:`cant update the task ${err}`
+        })
+    }
+}
 
 module.exports = {createTask,getTask,updateTask,deleteTask}
