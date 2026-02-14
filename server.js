017e4a8154cf7328a3bd852ff286783ecd37d861
NITHISHKUMAR0283 <nk0283@srmist.edu.in>
2026-02-14 12:49:38 +0530

    Added the Task route to the server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const TaskRoute = require("./src/routes/taskRoute")
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 app.get("/",(req,res)=>{
     console.log("sending res");
-    res.send("Hello world");
-});
+    res.send("Hello world");});
+app.get("/Task",TaskRoute);
 app.listen(PORT,()=>{
     console.log(`server is running in port ${PORT}`);
 })
