1836ec4302ee68b303179a227f71a49ac4005aaa
NITHISHKUMAR0283 <nk0283@srmist.edu.in>
2026-02-28 17:55:22 +0530

    added getoneproject function which will get one project by using id and user id to retreive respective user projects

--- src/controllers/projectcontroller.js
+++ src/controllers/projectcontroller.js
@@ -29,6 +29,26 @@ const getProject = async (req,res,next)=>{
             error:err
         })
     }
-
 }
-module.exports= {createProject,getProject};
+
+const getOneProject = async (req,res,next)=>{
+    try{
+    const project = await Project.findOne({owner:req.user._id,_id:req.params.id});
+    if(!project){
+        return res.status(404).json({
+            message:"Cant find a project by the given id",
+        })
+    }
+    res.status(200).json({
+        message:"Successfully found the project by id",
+        project_:project
+    })
+}
+    catch(err){
+        res.status(500).json({
+            message:"Error in finding the project by given id ",
+            error:err
+        })
+    }
+}
+module.exports= {createProject,getProject,getOneProject};
