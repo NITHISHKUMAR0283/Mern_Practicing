6d99c91dca57c4e331c0ebca604324ac0ec4f506
NITHISHKUMAR0283 <nk0283@srmist.edu.in>
2026-02-28 18:04:32 +0530

    added updateproject which uses the user id and project id to update the correct owner project

--- src/controllers/projectcontroller.js
+++ src/controllers/projectcontroller.js
@@ -51,4 +51,27 @@ const getOneProject = async (req,res,next)=>{
         })
     }
 }
-module.exports= {createProject,getProject,getOneProject};
+
+const updateProject = async (req,res,next)=>{
+    try{
+        const updated = await Project.findOneAndUpdate({owner:req.user._id,_id:req.params.id},req.body,{new:true,runValidators:true});
+        if(!updated){
+            return res.status(404).json({
+                message:"cant update the project"
+            }
+            )
+        }
+        res.status(200).json({
+            message:"Updation succesfull",
+            success:true,
+            updated_Project:updated
+        })
+    }catch(err){
+        res.status(500).json({
+            success:false,
+            message:"Error when updating the project"
+        }
+        )
+    }
+}
+module.exports= {createProject,getProject,getOneProject,updateProject};
