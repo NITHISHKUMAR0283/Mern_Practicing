2d121e13307c1e2a92b9ce22600afd07617276a4
NITHISHKUMAR0283 <nk0283@srmist.edu.in>
2026-03-08 05:42:40 +0530

    invoking cookie parser instead of passing as parameter

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 app.use(cors());
-app.use(cookieParser);
+app.use(cookieParser());
 app.use(express.json());
 connectDB();
 app.get("/",(req,res)=>{
--- src/middleware/authMiddleWare.js
+++ src/middleware/authMiddleWare.js
@@ -8,7 +8,7 @@ const isAuthenticated = async (req,res,next)=>{
     }
     const token = header.split(" ")[1];
     if (!token){
-        res.status(401).json({
+        return res.status(401).json({
             message:"No token provided"
         });
     }
@@ -16,7 +16,7 @@ const isAuthenticated = async (req,res,next)=>{
     const payload = jwt.verify(token,process.env.JWT_SECRET);
     const user = await User.findById(payload.id).select("-password");
     if (!user){
-        res.status(401).json({
+        return res.status(401).json({
             message:"User not found"
         })
     }
