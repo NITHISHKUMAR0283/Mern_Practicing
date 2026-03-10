ae546e877027250c0945cb1705a119cd4475b8b4
NITHISHKUMAR0283 <nk0283@srmist.edu.in>
2026-03-10 19:55:14 +0530

    Learned usestate by implementing dynamic count in the website

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import  {useState} from 'react';
 function App() {
   const [ name,setname]=useState("");
+  const [ count , setCount]=useState(0);
 
   return (
     <div style={{
@@ -17,7 +18,15 @@ function App() {
       <p style = {{marginTop:'20px',fontSize:'18px'}}>
         Hello,{name||'Guest'}!
       </p>
-
+      <button style={{height:"50px",width:"100px",border:"5px solid black",borderRadius:"50px",fontSize:"20px"}}
+        onClick={(e)=>setCount(count+1)
+        }
+        value = {count}>+1</button>
+      <button style={{height:"50px",width:"100px",border:"5px solid black",borderRadius:"50px",fontSize:"20px",margin:"10px"}}
+        onClick={(e)=>setCount(count-1)
+        }
+        value = {count}>-1</button>
+      <p>{count}</p>
     </div>
   );
 }
